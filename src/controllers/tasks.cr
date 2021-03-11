6bacd17cf59bbe0a5249ffcc02028d8756e69699
JulieGiles <julie_j_giles@hotmail.com>
2021-03-12 01:45:37 +1100

    Updated tasks controller to include access control allow methods

--- src/controllers/tasks.cr
+++ src/controllers/tasks.cr
@@ -1,18 +1,20 @@
 require "action-controller"
 require "../models/task.cr"
-require "xml"
 require "json" # https://crystal-lang.org/api/0.18.7/JSON/Builder.html
+require "http/client"
 
 class TasksController < ActionController::Base
+  #Root
   base "/tasks"
 
   before_action :find_task, only: [:show, :update, :destroy]
   getter :array_of_tasks, :task
 
+  #CRUD methods
   # GET /tasks/
   def index
     array_of_tasks = Task.query.select.to_a
-    render xml: array_of_tasks
+    render json: ({array_of_tasks, 200})
     # respond_with do
     #   html template("index.ecr")
     #   text "#{array_of_tasks}"
@@ -22,7 +24,7 @@ class TasksController < ActionController::Base
 
   # GET /tasks/:id
   def show
-    render xml: task
+    render json: ({task, 200})
     # respond_with do
     #   html template("show.ecr")
     #   text "#{task1}"
@@ -39,7 +41,7 @@ class TasksController < ActionController::Base
    
     new_task = Task.new(JSON.parse(request.body.as(IO)))
     new_task.save!
-    render xml: new_task
+    render json: ({new_task, 201})
     # respond_with do
     #   # html template("new_task.ecr")
     #   text "#{new_task}"
@@ -58,10 +60,19 @@ class TasksController < ActionController::Base
 
   # DELETE /tasks/:id
   def destroy
+    task.delete
+    render json: ({array_of_tasks, 200})
+    if !task.delete 
+      raise Exception.new("Could not delete task")
+    end
+
   end
 
+  #CORS - Allowed origins and allowed methods at which requests can be made
+  #https://iridakos.com/programming/2018/03/28/custom-http-headers
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+
   private def find_task
-    task = Task.find(params[:id])
-    # task = Task.query.where { raw("tasks.id") == :id }
+    task = Task.find(params[:id].to_i) || array_of_tasks.find( raw("tasks.id") == params[:id].to_i)  
   end
 end
